a0b29e47c8282dcec9c12c666079f4b964c1a6a2
alice <amw46@njit.edu>
2018-04-06 12:44:57 -0400

    category delete hidden form

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -15,12 +15,26 @@
 	   <td>
            <?php echo $category['categoryID']; ?>
 	   </td>
+        <form action="." method="post">
+            <input type="hidden" name="action"
+                   value="delete_category">
+            <input type="hidden" name="categoryName"
+                   value="<?php echo $category['categoryName']; ?>">
+            <input type="hidden" name="categoryID"
+                   value="<?php echo $category['categoryID']; ?>">
+            <input type="submit" value="Delete">
+        </form>
+        <td>
+
+        </td>
 	</tr> 
 	<?php endforeach; ?>
 
     </table>
 
     <p><a href="?action=show_add_category">Add A Category</a></p>
+
+    <p><a href="?action=show_add_product">Add A Product</a></p>
 
     <p><a href="index.php?action=list_products">List Products</a></p>
 
