bd5f309a98318ca857ea4d7ac655f36048775f8e
alice <amw46@njit.edu>
2018-04-06 12:36:14 -0400

    category delete added to category_db.php

--- model/category_db.php
+++ model/category_db.php
@@ -23,10 +23,22 @@ function get_category_name($category_id) {
 
 function add_category($categoryName, $categoryID) {
     global $db;
-    $query = 'INSERT INTO categories (categoryID, categoryName) VALUES (:categoryID, :categoryName)';
+    $query = 'INSERT INTO categories (categoryID, categoryName) 
+              VALUES (:categoryID, :categoryName)';
     $statement = $db->prepare($query);
     $statement->bindValue(':categoryID', $categoryID);
     $statement->bindValue(':categoryName', $categoryName);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function delete_category($categoryID) {
+    global $db;
+    $query = 'DELETE category
+                FROM categories 
+                WHERE categoryID = :categoryID';
+    $statement = $db->prepare($query);
+    $statement->bindValue( ':categoryID', $categoryID);
     $statement->execute();
     $statement->closeCursor();
 }
